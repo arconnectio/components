c0206d2a32e93856f307256c4da542703932c180
Marton Lederer <marton@lederer.hu>
2022-10-17 10:38:22 +0200

    fix: spacer

--- src/Spacer/index.tsx
+++ src/Spacer/index.tsx
@@ -1,9 +1,7 @@
 import styled from "styled-components";
 
-const Spacer = styled.span<{ x?: number; y?: number }>`
+export const Spacer = styled.span<{ x?: number; y?: number }>`
   display: block;
   height: ${({ y }) => (y ? `${y}rem` : "0")};
   width: ${({ x }) => (x ? `${x}rem` : "0")};
 `;
-
-export default Spacer;
